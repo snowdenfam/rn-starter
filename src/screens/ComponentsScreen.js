4ae3f0e18fef3c770fce19c052297defafa3f98e
Andre Snowden <andre.snowden92@gmail.com>
2023-03-15 13:15:20 -0400

    Rules of JSX

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
+  const greeting = 'Hi there!';
+  
+  // you cannot show a javascript object inside of a text element. For example const greeting = { color: 'red' }; wont show inside <Text>{greeting}</Text> ////
+
   return (
   <View><Text style={styles.textStyle}>This is the components screen. Wow, so many things can go wrong</Text>
-  <Text>Hi There</Text>
+  <Text>{greeting}</Text>
   </View> );
 };
 
