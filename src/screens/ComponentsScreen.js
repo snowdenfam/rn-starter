0c4d22b688406903c9796fc8ab170e8cda154cfc
Andre Snowden <andre.snowden92@gmail.com>
2023-03-15 13:48:40 -0400

    Add style to text element with variable

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -3,18 +3,38 @@ import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
   const greeting = 'Hi there!';
-  
+  const greeting3 = 'Andre Snowden';
+
   // you cannot show a javascript object inside of a text element. For example const greeting = { color: 'red' }; wont show inside <Text>{greeting}</Text> ////
 
   return (
-  <View><Text style={styles.textStyle}>This is the components screen. Wow, so many things can go wrong</Text>
-  <Text>{greeting}</Text>
+  <View><Text style={styles1.textStyle}>Getting started with React Native!</Text>
+
+
+
+  <Text style={styles3.textStyle}>My name is {greeting3}</Text>
   </View> );
 };
+
+////// This is how you style a text element that also has a variable in it with React Native //////////
+
+// Styling for text // 
 
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
+  },
+});
+
+const styles1 = StyleSheet.create({
+  textStyle: {
+    fontSize: 45,
+  },
+});
+
+const styles3 = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
   },
 });
 
