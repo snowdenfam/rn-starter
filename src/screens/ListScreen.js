75d02b2f006855eab7923f63c995a756eddd07d0
Andre Snowden <andre.snowden92@gmail.com>
2023-03-17 23:03:59 -0400

    Add age property to friend object

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,24 +3,24 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1', key: '1'},
-        {name: 'Friend #2', key: '2'},
-        {name: 'Friend #3', key: '3'},
-        {name: 'Friend #4', key: '4'},
-        {name: 'Friend #5', key: '5'},
-        {name: 'Friend #6', key: '6'},
-        {name: 'Friend #7', key: '7'},
-        {name: 'Friend #8', key: '8'},
-        {name: 'Friend #9', key: '9'},
+        {name: 'Friend #1', age: 20, key: '1'},
+        {name: 'Friend #2', age: 45, key: '2'},
+        {name: 'Friend #3', age: 32, key: '3'},
+        {name: 'Friend #4', age: 27, key: '4'},
+        {name: 'Friend #5', age: 53, key: '5'},
+        {name: 'Friend #7', age: 30, key: '7'},
+        
     ];
 
 return ( 
 <FlatList 
-  horizontal
-  showsHorizontalScrollIndicator={false}
   data={friends}
  renderItem={({ item }) => {
-   return <Text style={styles.textStyle}>{item.name}</Text>;
+   return (
+    <Text style={styles.textStyle}>
+    {item.name} - Age {item.age}
+</Text>
+   );
  }} 
 />
 );
