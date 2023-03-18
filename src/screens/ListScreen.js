4681b1031d0b4b8c1cd412bcbacaa822f62a8569
Andre Snowden <andre.snowden92@gmail.com>
2023-03-17 23:20:26 -0400

    Delete cumberson key ID

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,17 +3,18 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1', age: 20, key: '1'},
-        {name: 'Friend #2', age: 45, key: '2'},
-        {name: 'Friend #3', age: 32, key: '3'},
-        {name: 'Friend #4', age: 27, key: '4'},
-        {name: 'Friend #5', age: 53, key: '5'},
-        {name: 'Friend #7', age: 30, key: '7'},
+        {name: 'Friend #1', age: 20},
+        {name: 'Friend #2', age: 45},
+        {name: 'Friend #3', age: 32},
+        {name: 'Friend #4', age: 27},
+        {name: 'Friend #5', age: 53},
+        {name: 'Friend #7', age: 30},
         
     ];
 
 return ( 
 <FlatList 
+  keyExtractor={friend => friend.name}
   data={friends}
  renderItem={({ item }) => {
    return (
