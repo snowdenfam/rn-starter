77c6d6523816699047cc6f5bab8c1fb2c3d80684
Andre Snowden <andre.snowden92@gmail.com>
2023-03-20 23:35:29 -0400

    Cleaned up formatting

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
+    console.log(props);
   return ( 
   <View>
    <Image source={props.ImageSource} />
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -3,13 +3,21 @@ import { View, Text, StyleSheet } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-  return ( <View>
-<ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} 
+  return ( 
+  <View>
+<ImageDetail 
+title="Forest" 
+imageSource={require('../../assets/forest.jpg')} 
    />
-<ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} 
+<ImageDetail
+ title="Beach"
+  imageSource={require('../../assets/beach.jpg')} 
  />
-<ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} 
+ <ImageDetail
+  title="Mountain" 
+  imageSource={require('../../assets/mountain.jpg')} 
  />
+
    </View>
 );
 };
