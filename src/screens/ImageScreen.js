b8b2a6c99372eeabe64e4c8cfb39b560532b1a15
Andre Snowden <andre.snowden92@gmail.com>
2023-03-20 22:47:12 -0400

    Added props to ImageScreen.js

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -4,13 +4,10 @@ import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
   return ( <View>
-   <ImageDetail />
-   <ImageDetail />
-   <ImageDetail />
-   <ImageDetail />
-
-
-  </View>
+   <ImageDetail title="Forest" />
+   <ImageDetail title="Beach"  />
+   <ImageDetail title="Mountain"  />
+   </View>
 );
 };
 
