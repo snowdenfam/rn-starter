a7618b70bc8c9ce18ab205c12ce61ac92073d10e
Andre Snowden <andre.snowden92@gmail.com>
2023-03-21 18:57:07 -0400

    Add color box to ColorScreen.js

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -4,6 +4,8 @@ import { View, StyleSheet, Button } from 'react-native';
 const ColorScreen = () => {
   return ( <View>
    <Button title="Add a Color" />
+   <View 
+   style={{ height: 100, width: 100, backgroundColor: 'rgb(0, 255, 0)' }} />
 
   </View>
 
