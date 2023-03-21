2a3180e88a50ac5cf8caf0547f0394fce9725224
Andre Snowden <andre.snowden92@gmail.com>
2023-03-20 23:44:21 -0400

    Fixed prop name from ImageSource to imageSource

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = (props) => {
-    console.log(props);
+const ImageDetail = props => {
+    
   return ( 
   <View>
-   <Image source={props.ImageSource} />
+   <Image source={props.imageSource} />
 
   <Text>{props.title}</Text>
 
