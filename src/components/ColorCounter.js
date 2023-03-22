8c94f7b63682baee0b385321633c02f3e011a1b8
Andre Snowden <andre.snowden92@gmail.com>
2023-03-21 22:47:54 -0400

    Add ColorCounter boilerplate and buttons

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const ColorCounter = () => {
+ return (
+  <View>
+    <Text>Red</Text>
+    <Button title="Increase Red" />
+    <Button title="Decrease Red" />
+
+
+  </View>
+
+ );
+
+};
+
+const styles = StyleSheet.create({});
+
+
+export default ColorCounter;
