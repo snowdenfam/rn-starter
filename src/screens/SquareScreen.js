7896a43f75db843dd383e0c93356783dd3dd1380
Andre Snowden <andre.snowden92@gmail.com>
2023-03-24 23:30:56 -0400

    + onIncrease, onDecreatse callback functions

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,7 +6,8 @@ const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [blue, setBlue] = useState(0);
     const [green, setGreen] = useState(0);
-
+    
+console.log(red);
 
  return (
   <View>
