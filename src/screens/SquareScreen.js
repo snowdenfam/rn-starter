04fcdb641a0ce59363c052dcf15ba2bf11bd633d
Andre Snowden <andre.snowden92@gmail.com>
2023-03-26 18:41:13 -0400

    Make ColorCounter view uniform

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -34,13 +34,13 @@ const SquareScreen = () => {
   color="Red"
    />
   <ColorCounter 
-   onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-   onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+   onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+   onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
   color="Blue" 
   />
   <ColorCounter 
-   onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-   onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+   onIncrease={() => setColor('green', COLOR_INCREMENT)}
+   onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
    color="Green" 
   />
   <View style={{ 
