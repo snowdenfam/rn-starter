3d33cb0bba37b1ba6673c754d8e88ecf168ad704
Andre Snowden <andre.snowden92@gmail.com>
2023-03-27 00:28:56 -0400

    + return state default

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -12,17 +12,17 @@ const reducer = (state, action) => {
   switch (action.colorToChange)  {
    case 'red':
       if (state.red + action.amount > 255 || state.red + action.amount < 0) {
-         return;
+         return state;
       }
       return {...state, red: state.red + action.amount };
    case 'green':
       if (state.green + action.amount > 255 || state.green + action.amount < 0) {
-         return;
+         return state;
       }
       return {...state, green: state.green + action.amount };
    case 'blue':
       if (state.blue + action.amount > 255 || state.blue + action.amount < 0) {
-         return;
+         return state;
       }
       return {...state, blue: state.blue + action.amount };
    default:
