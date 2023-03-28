412f79d702cbbd6c1b583e1d4eb6c3079264d0e2
Andre Snowden <andre.snowden92@gmail.com>
2023-03-28 00:19:28 -0400

    + View with text

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const TextScreen = () => {
-
+  return <View>
+    <Text>Text Screen</Text>
+  </View>
 };
 
 const styles = StyleSheet.create ({});
