e08c472aaedbdd2f4ffc8024c08d8995ae748e8e
Andre Snowden <andre.snowden92@gmail.com>
2023-03-28 00:13:12 -0400

    + TextScreen boilerplate

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+
+};
+
+const styles = StyleSheet.create ({});
+
+export default TextScreen;
